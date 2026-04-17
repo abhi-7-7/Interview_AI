68bba91a21879bdc5e88d66a3d50b7e8469b7b1f
abhi-7-7 <aarsh4344@gmail.com>
2026-04-18 05:22:51 +0530

    fix(cors): improve allowed origins and preflight handling

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173" || process.env.VITE_FRONTEND_URL || "https://interview-ai-tawny.vercel.app/",
+    origin:["http://localhost:5173" , process.env.VITE_FRONTEND_URL].filter(Boolean),
     credentials:true
 }))
 
